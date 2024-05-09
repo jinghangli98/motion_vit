b9306cf215184ed45f0c11bac457a6aeeb2872f9
jinghangli98 <jil202@pitt.edu>
2024-05-08 23:25:42 -0400

    update

--- dataset.py
+++ dataset.py
@@ -1,7 +1,11 @@
 import torch
-import nibabel as nib
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, DataLoader, TensorDataset
 import torchio as tio
+import glob
+import pdb
+from sklearn.model_selection import train_test_split
+from tqdm import tqdm
+from torchvision.transforms import CenterCrop
 
 class tse_dataset(Dataset):
     def __init__(self, nii_dir, transform=None):
@@ -11,13 +15,20 @@ class tse_dataset(Dataset):
     def __len__(self):
         return len(self.nii_dir)
     
-    def __getitem__(self, idx):
-        
+    def __getitem__(self, idx):        
         pt = torch.load(self.nii_dir[idx])
-        img = pt['tse']
+        img = self.transform(torch.tensor(pt['slice']))
         rating = pt['rating']
         
-        return img, rating
-    
-dataset = tse_dataset(glob.glob('data/*.pt'))
+        return img/img.max(), rating
 
+        
+        
+        
+transform = CenterCrop((512, 512,))
+
+train_data, val_data = train_test_split(glob.glob('data/2d/*.pt'), test_size=0.2, random_state=42)
+train_dataset = tse_dataset(train_data, transform)
+val_dataset = tse_dataset(val_data, transform)
+train_loader = DataLoader(train_dataset, batch_size = 16, shuffle=False)
+test_loader = DataLoader(val_dataset, batch_size = 16, shuffle=False)
--- model.py
+++ model.py
@@ -2,20 +2,41 @@ import torch
 import torch.nn as nn
 from monai.networks.nets.vit import ViT
 from monai.networks.nets import ViTAutoEnc
-
+import monai
 import pdb
 
-model = ViTAutoEnc(
-    in_channels=1,
-    img_size=(512, 512, 48),
-    patch_size=(16, 16, 16),
-    pos_embed="conv",
-    hidden_size=768,
-    mlp_dim=3072,
-    save_attn=True
-).to('cuda')
+# model = ViTAutoEnc(
+#     in_channels=1,
+#     img_size=(464, 464, 48),
+#     patch_size=(16, 16, 16),
+#     pos_embed="conv",
+#     hidden_size=768,
+#     mlp_dim=3072,
+#     save_attn=True
+# )
+# model = ViT(in_channels=1, 
+#             img_size=(464, 464, 48), 
+#             patch_size=(16, 16, 16), 
+#             proj_type='conv', 
+#             pos_embed_type='sincos',
+#             save_attn=True,
+#             classification=True,
+#             num_classes = 5)
+# model = monai.networks.nets.DenseNet121(spatial_dims=3, in_channels=1, out_channels=5).to('cuda')
+model = monai.networks.nets.DenseNet121(spatial_dims=2, in_channels=1, out_channels=5).to('cuda')
+# model = monai.networks.nets.TorchVisionFCModel(model_name='resnet18', num_classes=5, dim=2, in_channels=None)
+# model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet34', pretrained=False).to('cuda')
+# original_first_layer = model.conv1
+# model.conv1 = torch.nn.Conv2d(1, original_first_layer.out_channels,
+#                               kernel_size=original_first_layer.kernel_size, 
+#                               stride=original_first_layer.stride, 
+#                               padding=original_first_layer.padding, 
+#                               bias=original_first_layer.bias)
+# num_ftrs = model.fc.in_features
+# model.fc = torch.nn.Linear(num_ftrs, 5)
 
-pdb.set_trace()
-img = torch.rand((1, 1, 512, 512, 48)).to('cuda')
-out, hidden = model(img)
-torch.stack(hidden)
+# model = model.to('cuda')
+# img = torch.rand((1, 1, 512, 512, 48)).to('cuda')
+# out, hidden = model(img)
+# pdb.set_trace()
+# torch.stack(hidden)
